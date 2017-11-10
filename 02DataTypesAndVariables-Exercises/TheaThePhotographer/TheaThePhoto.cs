68c5cf459a773e7bcb8c0da7e8ff5a7286c2cf06
Deyan <dlucky95@gmail.com>
2017-11-11 01:44:46 +0200

    Thea 40/100

--- 02DataTypesAndVariables-Exercises/TheaThePhotographer/TheaThePhoto.cs
+++ 02DataTypesAndVariables-Exercises/TheaThePhotographer/TheaThePhoto.cs
@@ -10,6 +10,22 @@ namespace TheaThePhotographer
     {
         static void Main(string[] args)
         {
+            int numberOfPics = int.Parse(Console.ReadLine());
+            int filterTime = int.Parse(Console.ReadLine());
+            int filterFactor = int.Parse(Console.ReadLine());
+            int uploadTime = int.Parse(Console.ReadLine());
+
+            decimal usefulPics = (numberOfPics * filterFactor) / 100;
+            usefulPics = Math.Ceiling(usefulPics);
+            int totalProcessingTime = numberOfPics * filterTime;
+            decimal totalUploadTime = (usefulPics) * uploadTime;
+
+            int total = totalProcessingTime + (int)totalUploadTime;
+
+            TimeSpan time = TimeSpan.FromSeconds(total);
+
+            string str = time.ToString(@"d\:hh\:mm\:ss");
+            Console.WriteLine(str);
         }
     }
 }
