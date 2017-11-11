57fe410687954855549e246078a3f649cc6473af
Deyan <dlucky95@gmail.com>
2017-11-11 12:54:07 +0200

    03 Methods and Debugging

---
+++ 03MethodsAndDebugging/PrintingTriangle/PrintTriangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingTriangle
+{
+    class PrintTriangle
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < input+1; i++)
+            {
+                PrintLine(1, i);
+            }
+
+            //PrintLine(1, input);
+
+            for (int i = input - 1; i >= 0; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+
+        static void PrintLine(int start, int number)
+        {
+            for (int i = start; i <= number; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+
+    }
+}
---
+++ 03MethodsAndDebugging/SignOfInteger/SignOfInt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignOfInteger
+{
+    class SignOfInt
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            PrintIntSign(n);
+        }
+
+        static void PrintIntSign(int num)
+        {
+            if (num > 0) Console.WriteLine($"The number {num} is positive.");
+            else if (num < 0) Console.WriteLine($"The number {num} is negative.");
+            else Console.WriteLine($"The number {num} is zero.");
+        }
+    }
+}
