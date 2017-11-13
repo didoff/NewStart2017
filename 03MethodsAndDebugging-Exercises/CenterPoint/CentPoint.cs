3b254c4b3d2fbc427b024a1324ce5dc2bfc22201
Deyan <dlucky95@gmail.com>
2017-11-13 19:45:39 +0200

    CentPoint improvement

---
+++ 03MethodsAndDebugging-Exercises/CenterPoint/CentPoint.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenterPoint
+{
+    class CentPoint
+    {
+        // 80/100 in Judge; 
+        // better stuff: https://pastebin.com/nMjTaqVU
+
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            GetCenterPoint(x1, y1, x2, y2);
+        }
+
+        private static void GetCenterPoint(double x1, double y1, double x2, double y2)
+        {
+            if (Math.Abs(x1) < Math.Abs(x2) && Math.Abs(y1) < Math.Abs(y2))
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else if (Math.Abs(x1) > Math.Abs(x2) && Math.Abs(y1) > Math.Abs(y2))
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+            else if (Math.Abs(x1 + y1) < Math.Abs(x2 + y2))
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else if (Math.Abs(x1 + y1) > Math.Abs(x2 + y2))
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+        }
+    }
+}
---
+++ 03MethodsAndDebugging-Exercises/CenterPointV2/CentPointV2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenterPointV2
+{
+    class CentPointV2
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            PrintPointClosestToOrigin(x1, y1, x2, y2);
+
+        }
+
+        private static void PrintPointClosestToOrigin(double x1, double y1, double x2, double y2)
+        {
+            double distance1 = Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
+            double distance2 = Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2, 2));
+
+            if (distance1 <= distance2)
+            {
+                Console.WriteLine($"({x1}, {y1})");
+            }
+            else
+            {
+                Console.WriteLine($"({x2}, {y2})");
+            }
+
+        }
+    }
+}
