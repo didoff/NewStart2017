d7d9f53b548a024d38d6c30c395cf53787d74b23
Deyan <dlucky95@gmail.com>
2017-11-17 16:59:32 +0200

    arr and lists ext

---
+++ 04ArraysAndLists/LastKNumbersSequence/LastKnumSeq.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LastKNumbersSequence
+{
+    class LastKnumSeq
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            PrintKSeqence(n, k);
+        }
+
+        private static void PrintKSeqence(int n, int k)
+        {
+            int[] seq = new int[n];
+            seq[0] = 1;
+
+            for (int i = 1; i < seq.Length; i++)
+            {
+                //int sum = 0;
+
+                for (int j = 0; j < i - k; j++)
+                {
+                    seq[i] += seq[i - k];
+                }
+                Console.WriteLine(seq[i]);
+            }
+        }
+    }
+}
