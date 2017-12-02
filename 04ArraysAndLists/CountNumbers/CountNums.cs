8dc30d82e47877d453248c841f00a9a7b309308f
Deyan <dlucky95@gmail.com>
2017-12-02 21:00:09 +0200

    End, but still more work to do

---
+++ 04ArraysAndLists/CountNumbers/CountNums.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountNumbers
+{
+    class CountNums
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine().Split().Select(a => int.Parse(a)).ToList();
+            input.Sort();
+
+            int[] counts = new int[input.Max() + 1];
+
+            foreach (var num in input)
+            {
+                counts[num]++;
+            }
+
+            for (int i = 0; i < counts.Length; i++)
+            {
+                if(counts[i] > 0)
+                {
+                    Console.WriteLine($"{i} -> {counts[i]}");
+                }
+            }
+
+            // Explore more ways to solve this.
+        }
+    }
+}
