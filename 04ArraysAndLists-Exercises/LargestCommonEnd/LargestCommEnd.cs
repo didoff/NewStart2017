45f02c0f3bc9f47e0b5c3062947ee66bef87c5d9
Deyan <dlucky95@gmail.com>
2017-12-22 13:22:30 +0200

    un

---
+++ 04ArraysAndLists-Exercises/LargestCommonEnd/LargestCommEnd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestCommonEnd
+{
+    class LargestCommEnd
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split().ToArray();
+            string[] secondInput = Console.ReadLine().Split().ToArray();
+
+            int countLeft = 0;
+            int countRight = 0;
+
+            while()
+
+            if (countLeft > countRight)
+                Console.WriteLine(countLeft);
+            else
+                Console.WriteLine(countRight);
+        }
+    }
+}
