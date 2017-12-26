7a22170a1994c8152f61348c926dd86727692db9
Deyan <dlucky95@gmail.com>
2017-12-26 20:31:59 +0200

    Largest Common End

--- 04ArraysAndLists-Exercises/LargestCommonEnd/LargestCommEnd.cs
+++ 04ArraysAndLists-Exercises/LargestCommonEnd/LargestCommEnd.cs
@@ -12,16 +12,45 @@ namespace LargestCommonEnd
         {
             string[] firstInput = Console.ReadLine().Split().ToArray();
             string[] secondInput = Console.ReadLine().Split().ToArray();
+            //secondInput.Reverse();
 
             int countLeft = 0;
             int countRight = 0;
+            int currentPosition = 0;
 
-            while()
+            int maxArrLen = Math.Max(firstInput.Length, secondInput.Length);
+            int minArrLen = Math.Min(firstInput.Length, secondInput.Length);
 
-            if (countLeft > countRight)
-                Console.WriteLine(countLeft);
-            else
+            for (int i = 0; i < minArrLen; i++)
+            {
+                if (firstInput[i] == secondInput[i])
+                {
+                    currentPosition = i;
+                    countLeft++;
+                }
+            }
+
+            string[] revFirst = firstInput.Reverse().ToArray();
+            string[] revSecond = secondInput.Reverse().ToArray();
+
+            for (int i = 0; i < minArrLen; i++)
+            {
+                if (revFirst[i] == revSecond[i])
+                {
+                    currentPosition = i;
+                    countRight++;
+                }
+            }
+
+            if (countRight > countLeft)
+            {
                 Console.WriteLine(countRight);
+            }
+            else
+            {
+                Console.WriteLine(countLeft);
+            }
+
         }
     }
 }
