55b1e226ffefd9a96eaca8514208d513e01cbc87
Deyan <dlucky95@gmail.com>
2017-12-26 21:26:00 +0200

    Fold done, Rotate pending

---
+++ 04ArraysAndLists-Exercises/FoldAndSum/FoldSum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldAndSum
+{
+    class FoldSum
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(a => int.Parse(a)).ToArray();
+            int k = input.Length / 4;
+
+            int[] left = input.Take(k).ToArray();
+            int[] right = input.Reverse().Take(k).ToArray();
+            int[] center = input.Skip(k).Take(2 * k).ToArray();
+
+            int[] topRow = left.Reverse().Concat(right).ToArray();
+            int[] result = new int[2 * k];
+
+            for (int i = 0; i < 2 * k; i++)
+            {
+                result[i] = topRow[i] + center[i];
+            }
+
+            foreach (var sum in result)
+            {
+                Console.Write(sum + " ");
+            }
+        }
+    }
+}
---
+++ 04ArraysAndLists-Exercises/RotataAndSum/RotateSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotataAndSum
+{
+    class RotateSum
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(a => int.Parse(a)).ToArray();
+            int k = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < k - 1; i++)
+            {
+                for (int j = 0; j < input.Length - 1; j++)
+                {
+
+                }
+            }
+
+        }
+    }
+}
